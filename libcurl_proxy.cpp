dc2614707c6dabb4b2c5c04bd02af8db23b61fb1
faterzz <83334666+faterzz@users.noreply.github.com>
2022-06-09 11:14:19 +0800

    Update libcurl_proxy.cpp

--- libcurl_proxy.cpp
+++ libcurl_proxy.cpp
@@ -24,44 +24,46 @@ static PCHAR WideStrToMultiStr(PWCHAR WideStr)
 	return MultiStr;
 }
 	
-QString coverPCharToString(LPWSTR pStr)	
+QString covertPCharToString(LPWSTR pStr)	
 {
     PCHAR charstr1 = WideStrToMultiStr(pStr);
     QString resString = QString::fromLocal8Bit(charstr1);
 	return resString;
 }
 
-WINHTTP_CURRENT_USER_IE_PROXY_CONFIG pProxyConfig;
-pProxyConfig.fAutoDetect = TRUE;
-bool bState = WinHttpGetIEProxyConfigForCurrentUser(&pProxyConfig);
-if (bState)
+void setIEProxy() 
 {
-	DWORD Err = GetLastError();
-	qDebug() << "check the error :" << Err;
-	qDebug() << "ProxyConfig.lpszProxy" << pProxyConfig.fAutoDetect << pProxyConfig.lpszAutoConfigUrl << pProxyConfig.lpszProxy<< pProxyConfig.lpszProxyBypass;
-
-	if (pProxyConfig.lpszProxy != nullptr)
+	WINHTTP_CURRENT_USER_IE_PROXY_CONFIG pProxyConfig;
+	pProxyConfig.fAutoDetect = TRUE;
+	bool bState = WinHttpGetIEProxyConfigForCurrentUser(&pProxyConfig);
+	if (bState)
 	{
-		PCHAR charstr1 = WideStrToMultiStr(pProxyConfig.lpszProxy);
-		PCHAR charstr2 = WideStrToMultiStr(pProxyConfig.lpszProxyBypass);
-		QString str1 = QString::fromLocal8Bit(charstr1);
-		QString str2 = QString::fromLocal8Bit(charstr2);
-		qDebug() << "charstr1r :" << str1 << str2;
-		QStringList httpProxyStrList = str1.split(';');
-		qDebug() << "proxyAddr" << httpProxyStrList;
-		foreach (QString httpProxyStr, httpProxyStrList) {
-			if (httpProxyStr.startsWith("http=")) {
-				QString httpStr = httpProxyStr.remove("http=");
-				curl_easy_setopt(easy_handle, CURLOPT_PROXY, httpStr.toStdString().c_str()); //代理服务器地址//MyProxyConfig.lpszProxy /"127.0.0.1:11359"
-				curl_easy_setopt(easy_handle, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //设置http的代理类型
-				//curl_easy_setopt(easy_handle, CURLOPT_PROXYUSERPWD, "user:password");
-			} else if (httpProxyStr.startsWith("https=")) {
-				QString httpStr = httpProxyStr.remove("https=");
-				//curl_easy_setopt(easy_handle, CURLOPT_PROXY, httpStr.toStdString().c_str()); //代理服务器地址//MyProxyConfig.lpszProxy /"127.0.0.1:11359"
-				//curl_easy_setopt(easy_handle, CURLOPT_PROXYTYPE, CURLPROXY_HTTPS); //设置http的代理类型
+		DWORD Err = GetLastError();
+		qDebug() << "check the error :" << Err;
+		qDebug() << "ProxyConfig.lpszProxy" << pProxyConfig.fAutoDetect << pProxyConfig.lpszAutoConfigUrl << pProxyConfig.lpszProxy<< pProxyConfig.lpszProxyBypass;
+
+		if (pProxyConfig.lpszProxy != nullptr)
+		{
+			QString strProxy = covertPCharToString(pProxyConfig.lpszProxy);
+			QString strProxyBypass = covertPCharToString(pProxyConfig.lpszProxyBypass);
+			qDebug() << "charstr1r :" << strProxy << strProxyBypass;
+			QStringList httpProxyStrList = strProxy.split(';');
+			qDebug() << "proxyAddr" << httpProxyStrList;
+			foreach (QString httpProxyStr, httpProxyStrList) {
+				if (httpProxyStr.startsWith("http=")) {
+					QString httpStr = httpProxyStr.remove("http=");
+					curl_easy_setopt(easy_handle, CURLOPT_PROXY, httpStr.toStdString().c_str()); //代理服务器地址//lpszProxy //"127.0.0.1:11359"
+					curl_easy_setopt(easy_handle, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //设置http的代理类型
+					//curl_easy_setopt(easy_handle, CURLOPT_PROXYUSERPWD, "user:password");
+				} else if (httpProxyStr.startsWith("https=")) {
+					QString httpStr = httpProxyStr.remove("https=");
+					//curl_easy_setopt(easy_handle, CURLOPT_PROXY, httpStr.toStdString().c_str()); 
+					//curl_easy_setopt(easy_handle, CURLOPT_PROXYTYPE, CURLPROXY_HTTPS); //设置http的代理类型
+				}
 			}
+			curl_easy_setopt(easy_handle, CURLOPT_HTTPPROXYTUNNEL, 1L); //若是http请求，设置通道
 		}
-		curl_easy_setopt(easy_handle, CURLOPT_HTTPPROXYTUNNEL, 1L); //若是https请求，设置通道
+		//WinHttpGetProxyForUrl(url.toEncoded().toStdString().c_str());
 	}
-	//WinHttpGetProxyForUrl(url.toEncoded().toStdString().c_str());
 }
+
